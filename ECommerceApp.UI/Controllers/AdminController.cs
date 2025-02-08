1f5182b3f190f05e956e067a337d9d6f49fc6567
Benovshe313 <b.mahalova@gmail.com>
2025-02-08 21:37:32 +0400

    Add project files.

---
+++ ECommerceApp.UI/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            TempData["user"] = User.Identity.Name;
+            return View();
+        }
+    }
+}
---
+++ ECommerceApp.UI/Models/ShippingDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ECommerce.Entities.Concrete;
+
+namespace ECommerceApp.UI.Models
+{
+    public class ShippingDetailsViewModel
+    {
+        public ShippingDetails ShippingDetails { get; internal set; }
+    }
+}
